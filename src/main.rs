8feb47300563fbf050e7380d0ae8d0b4b7185fde
FarerABR <a.baghaee81@outlook.com>
2023-03-14 19:23:01 +0330

    DONE - even order square

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,17 @@ fn main() {
 
     #[allow(unused_assignments)]
     let mut square = Vec::new();
+
     if n % 2 == 1 {
         square = odd_square(n);
     } else if n % 4 == 0 {
-        square = d_even_square(n);
+        square = doubly_even_square(n);
     } else {
-        square = s_even_square(n);
+        square = singly_even_square(n);
     }
-    let dec = number_of_dec(&(n * n));
-    show_square(&square, dec);
+
+    check_magic(&square);
+    show_square(&square, number_of_dec(&(n * n)));
 }
 
 fn odd_square(n: i32) -> Vec<Vec<i32>> {
@@ -46,7 +48,7 @@ fn odd_square(n: i32) -> Vec<Vec<i32>> {
     sq
 }
 
-fn d_even_square(n: i32) -> Vec<Vec<i32>> {
+fn doubly_even_square(n: i32) -> Vec<Vec<i32>> {
     let mut out: Vec<Vec<i32>> = vec![vec![0; n as usize]; n as usize];
     (0..n).for_each(|i| {
         (0..n).for_each(|j| {
@@ -84,23 +86,127 @@ fn d_even_square(n: i32) -> Vec<Vec<i32>> {
         })
     });
     out
-    // todo!()
 }
 
-fn s_even_square(n: i32) -> Vec<Vec<i32>> {
-    todo!()
+fn singly_even_square(n: i32) -> Vec<Vec<i32>> {
+    let mut out: Vec<Vec<i32>> = vec![vec![0; n as usize]; n as usize];
+
+    // Top left
+    fill_quarter(&mut out, 0, n / 2, 0, n / 2, 1, (n / 2) * (n / 2));
+
+    // Bottom right
+    fill_quarter(
+        &mut out,
+        n / 2,
+        n,
+        n / 2,
+        n,
+        (n / 2) * (n / 2) + 1,
+        n * n / 2,
+    );
+
+    // Top right
+    fill_quarter(
+        &mut out,
+        0,
+        n / 2,
+        n / 2,
+        n,
+        n * n / 2 + 1,
+        3 * (n / 2) * (n / 2),
+    );
+
+    // Bottom left
+    fill_quarter(
+        &mut out,
+        n / 2,
+        n,
+        0,
+        n / 2,
+        3 * (n / 2) * (n / 2) + 1,
+        n * n,
+    );
+
+    let shift = (n / 2 - 1) / 2;
+    for i in 0..n / 2 {
+        for j in 0..shift {
+            if i == n / 4 {
+                exchange_cell(i, j + shift, &mut out);
+            } else {
+                exchange_cell(i, j, &mut out);
+            }
+        }
+    }
+    for i in 0..n / 2 {
+        for j in (n - shift)..(n - 1) {
+            exchange_cell(i, j, &mut out);
+        }
+    }
+    out
+}
+
+fn fill_quarter(
+    sq: &mut Vec<Vec<i32>>,
+    f_row: i32,
+    l_row: i32,
+    f_col: i32,
+    l_col: i32,
+    f_num: i32,
+    l_num: i32,
+) {
+    let mut num = f_num;
+    let mut i = f_row;
+    let mut j = (l_col + f_col) / 2;
+
+    while num <= l_num {
+        if i < f_row && j >= l_col {
+            i += 2;
+            j -= 1;
+        };
+        if j >= l_col {
+            j = f_col;
+        }
+        if i < f_row {
+            i = l_row - 1;
+        }
+        if sq[i as usize][j as usize] != 0 {
+            i += 2;
+            j -= 1;
+            continue;
+        } else {
+            sq[i as usize][j as usize] = num;
+            num += 1;
+        }
+        i -= 1;
+        j += 1;
+    }
+}
+fn exchange_cell(i: i32, j: i32, sq: &mut Vec<Vec<i32>>) {
+    let k = sq.len() / 2 + i as usize;
+    let tmp = sq[i as usize][j as usize];
+    sq[i as usize][j as usize] = sq[k][j as usize];
+    sq[k][j as usize] = tmp;
 }
 
 fn show_square(sq: &Vec<Vec<i32>>, dec: i32) {
     (0..sq.len()).for_each(|i| {
         (0..sq.len()).for_each(|j| {
-            print!("{}", sq[i][j]);
+            print!("{}  ", sq[i][j]);
             (0..=(dec as i32 - number_of_dec(&sq[i][j])).abs()).for_each(|_| {
                 print!(" ");
             })
         });
         println!("\n");
     })
+}
+
+fn check_magic(sq: &Vec<Vec<i32>>) {
+    if sq.iter().fold(0, |acc, i| acc + i[0]) != sq.iter().fold(0, |acc, i| acc + i[1])
+        || sq.iter().fold(0, |acc, i| acc + i[sq.len() - 1])
+            != sq.iter().fold(0, |acc, i| acc + i[sq.len() - 2])
+    {
+        eprintln!("THE SQUARE IS NOT MAGIC");
+    }
 }
 
 fn number_of_dec(input: &i32) -> i32 {
